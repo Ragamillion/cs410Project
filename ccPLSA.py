ab9071238ea78427911332609a58a60ab4c2915c
tphelan <tphelan3@illinois.edu>
2018-12-08 15:22:20 -0600

    Completed EM function

--- ccPLSA.py
+++ ccPLSA.py
@@ -6,11 +6,11 @@ Created on Sat Nov 17 14:59:26 2018
 """
 import numpy
 import pandas
-import nltk
-import collections
-from collections import Counter
-import time
-import scipy.sparse
+#import nltk
+#import collections
+#from collections import Counter
+#import time
+#import scipy.sparse
 from sklearn.feature_extraction.text import CountVectorizer
 #nltk.download('punkt')
 #nltk.download('stopwords')
@@ -31,7 +31,7 @@ class InputFile(object):
 
         contextList = ""
 
-    def __init__(self, filepath, documentCol, contextDiv, stopwords):
+    def __init__(self, filepath, documentCol, contextDiv, stopwords, nTopics):
         # initialize the object. Here filepath is the path to the data file, documentCol is the column in which the documents
         # are found, contextDiv is an array of the columns that determine the contexts, and stopwords is a .txt file containing
         # the stopwords
@@ -43,7 +43,7 @@ class InputFile(object):
         self.stopwords = stopwords
 
         self.nTopics = nTopics
-        self.backgroundLambda = backgroundLambda
+        #self.backgroundLambda = backgroundLambda
         
     def buildContext(self):
         #builds the list of contexts, and creates a new column in the dataframe that contains the combined context for each
@@ -56,12 +56,21 @@ class InputFile(object):
         docfile["plsaContext"] = ""
         contextDiv = pandas.Series(["account_category","dateSent"])
         
+        #concatenates the values in all the specified context columns for each row/document
         for context in contextDiv:
             docfile["plsaContext"] = docfile["plsaContext"] + "|" + docfile[context] 
         
+        #remove leading "|"
         docfile["plsaContext"] = docfile["plsaContext"].str[1:]
-        contextList = docfile["plsaContext"].unique()
         
+        #gets list of unique contexts
+        contextList = docfile["plsaContext"].unique().tolist()
+        
+        #Adds column for the index number of the context, for use in future looping
+        docfile["contextNum"] = docfile["plsaContext"].apply(lambda x: contextList.index(x))
+        
+        #Add on the "Common" context that will be used in conjunction with the separate contexts
+        contextList.append("Common")
 
 
     def prepareDocs(self):
@@ -107,7 +116,8 @@ class InputFile(object):
         but all were too slow.
 
         '''
-        #using sklearn data types 
+        #using sklearn data types. The docMat is a sparse matrix of word counts by document.
+        #The fns is a unique list of words.
         
         docVectors = CountVectorizer()
         
@@ -123,11 +133,11 @@ class InputFile(object):
         # background dataset is taken from http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
         #Used in the paper by Go, A., Bhayani, R. and Huang, L., 2009. 
         #"Twitter sentiment classification using distant supervision". CS224N Project Report, Stanford, 1(12).
-        backgroundDocs = pandas.read_csv('background tweets.csv')
+        #backgroundDocs = pandas.read_csv('background tweets.csv')
         
-        backgroundTokens = nltk.tokenize.casual.casual_tokenize(backgroundDocs['text'].str.decode('utf-8').str.lower().str.cat(sep=' '))
+        #backgroundTokens = nltk.tokenize.casual.casual_tokenize(backgroundDocs['text'].str.decode('utf-8').str.lower().str.cat(sep=' '))
         
-        bfreq = nltk.FreqDist(backgroundTokens)
+        #bfreq = nltk.FreqDist(backgroundTokens)
         
         #bfreq.freq('the')
         
@@ -137,12 +147,15 @@ class InputFile(object):
         
              
         nTopics = 4 #for testing purposes
-        backgroundLambda = .2 #for testing purposes
+        #backgroundLambda = .2 #for testing purposes
+        collectionLambda = .25
+        nccollectionLambda = .75
+        commonIndex = contextList.index("Common")
         
         #build random arrays to initiate
         pDocTopic = numpy.random.random(size = (len(docfile), nTopics))
-        pWordTopic = numpy.random.random(size = (len(fns), nTopics))
-        pWordDocTopic = numpy.random.random(size = (len(docfile),len(fns), nTopics))
+        pWordTopic = numpy.random.random(size = (len(fns), len(contextList), nTopics))
+        pWordDocTopic = numpy.random.random(size = (len(docfile),len(fns), len(contextList), nTopics))
         
         #normalize so that sum of p = 1
         for doc in range(pDocTopic.shape[0]):
@@ -151,39 +164,61 @@ class InputFile(object):
                 pDocTopic[doc, tp] /= nm
         
         for wd in range(pWordTopic.shape[0]):
-            nm = numpy.linalg.norm(pWordTopic[wd], ord=1)
-            for tp in range(nTopics):
-                pWordTopic[wd, tp] /= nm
-        
+            for cn in range(len(contextList)):
+                nm = numpy.linalg.norm(pWordTopic[wd][cn], ord=1)
+                for tp in range(nTopics):
+                    pWordTopic[wd, cn, tp] /= nm		
+		
+        #E-Step: n is doc, x is word
+        #This calculates the probabilities for the "hidden Z variable" for the 
+        #specific context and the Common context. Results are then used in the
+        #M-Step.
         for n in range(pDocTopic.shape[0]):
             for x in docMat[n].nonzero()[1]:
-                probVec = pDocTopic[n] * pWordTopic[x]
+                step1 = (collectionLambda * pWordTopic[x][commonIndex] 
+                    + (nccollectionLambda * pWordTopic[x][docfile["contextNum"][n]]))
+                probVec = pDocTopic[n] * step1
                 nm = numpy.linalg.norm(probVec, ord=1)
-                pWordDocTopic[n][x] = probVec/nm
+                pWordDocTopic[n][x][docfile["contextNum"][n]] = probVec/nm
+                pWordDocTopic[n][x][commonIndex] = (collectionLambda * pWordTopic[x][commonIndex])/step1
         
+        #M-Step: z is topic, n is doc, x is word. S is the specific context, t is the Common context
+        #this calculates new probabilites for the individual words in each topic.
         for z in range(nTopics):
             for n in range(pDocTopic.shape[0]):
                 s = 0
+                t = 0
                 for x in docMat[n].nonzero()[1]:
                     count = docMat[n,x]
-                    s = s + count * pWordDocTopic[n][x][z]
-                pWordTopic[x][z] = s
-            nm = numpy.linalg.norm(pWordTopic[z], ord=1)
-            pWordTopic[z] = pWordTopic[z]/nm
+                    s = s + count * pWordDocTopic[n][x][docfile["contextNum"][n]][z] * (1-pWordDocTopic[n][x][commonIndex][z])
+                    t = t + count * pWordDocTopic[n][x][commonIndex][z] * pWordDocTopic[n][x][docfile["contextNum"][n]][z]
+                pWordTopic[x][docfile["contextNum"][n]][z] = s
+                pWordTopic[x][commonIndex][z] = t
+            nm = numpy.linalg.norm(pWordTopic[:,:,z], ord=1)
+            pWordTopic[:,:,z] = pWordTopic[:,:,z]/nm
         
+        #Second part of the M-Step: n is doc, z is topic. s is to calculate the 
+        #document-specific mixing weight.
         for n in range(pDocTopic.shape[0]):
             for z in range(nTopics):
                 s = 0
                 for x in docMat[n].nonzero()[1]:
                     count = docMat[n,x]
-                    s = s + count * pWordDocTopic[n][x][z]
+                    s = s + count * pWordDocTopic[n][x][z][docfile["contextNum"][n]]
                 pDocTopic[n][z] = s
             nm = numpy.linalg.norm(pDocTopic[z], ord=1)
             pDocTopic[z] = pDocTopic[z]/nm
+       
         
-        #for iteration in range(10):
-        #    print "Iteration #" + str(iteration + 1) + "..."
-    
+        #To print top 10 words in all contexts/topics
+        for z in range(nTopics):
+            print("Topic " + str(z))
+            for c in range(len(contextList)):
+                print("context is " + contextList[c])
+                slist = sorted(range(len(pWordTopic)), key=lambda i: pWordTopic[i,c,z], reverse=True)[:10]
+                for word in slist:
+                    print(fns[word])
+          
 
         #wordFrame.apply(lambda x: pandas.value_counts(x.split(" "))).sum(axis=0)
 
